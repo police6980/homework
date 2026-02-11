c3f4a33588fbbe16fd8ffc8d75a10b0a15f34b6c
Homework Student <student@homework.app>
2026-02-11 12:43:27 +0900

    Configure for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -149,7 +149,11 @@ app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
 
